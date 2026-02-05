ed7820e3fe07d0a8ab5214a6d138b7284a6f9df5
IrfanCatovic <catovicc84@gmail.com>
2026-02-05 17:49:50 +0100

    feat: add uploader component for bulk data management and new cabin features
    - Implemented Uploader component for managing sample data uploads including guests, cabins, and bookings.
    - Created AddCabin component for adding new cabins via a modal.
    - Added CabinTableOperations component for filtering and sorting cabin data.
    - Developed Stats component to display key metrics related to bookings and occupancy.
    - Introduced TodayItem component to show today's check-in and check-out actions with appropriate buttons.

--- src/App.jsx
+++ src/App.jsx
@@ -15,22 +15,22 @@ function App() {
   return (
     <>
       <GlobalStyles />
-        <BrowserRouter>
-          <Routes>
-            <Route element={<AppLayout />}>
-              <Route index element={<Navigate replace to="dashboard" />} />
-              <Route path="dashboard" element={<Dashboard />} />
-              <Route path="bookings" element={<Bookings />} />
-              <Route path="cabins" element={<Cabins />} />
-              <Route path="users" element={<Users />} />
-              <Route path="settings" element={<Settings />} />
-              <Route path="account" element={<Account />} />
-            </Route>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<AppLayout />}>
+            <Route index element={<Navigate replace to="dashboard" />} />
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="bookings" element={<Bookings />} />
+            <Route path="cabins" element={<Cabins />} />
+            <Route path="users" element={<Users />} />
+            <Route path="settings" element={<Settings />} />
+            <Route path="account" element={<Account />} />
+          </Route>
 
-            <Route path="login" element={<Login />} />
-            <Route path="*" element={<PageNotFound />} />
-          </Routes>
-        </BrowserRouter>
+          <Route path="login" element={<Login />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
